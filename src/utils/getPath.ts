d4b477cbee9a460658909d0e08ab28cce9a204e6
Eng. Ahmed Abdelfattah <92686018+ENGAhmedAbdelfattah@users.noreply.github.com>
2023-08-13 10:17:07 +0300

    last update

--- src/utils/getPath.ts
+++ src/utils/getPath.ts
@@ -2,16 +2,16 @@ import { lang } from "@/types/lang";
 import { headers } from "next/headers";
 
 function getPath() {
-    const headersList = headers();
-	  const pathname = headersList.get("x-invoke-path") || "";
-    const purePath = pathname.startsWith("/en")
+  const headersList = headers();
+  const pathname = headersList.get("x-invoke-path") || "";
+  const purePath = pathname.startsWith("/en")
     ? pathname.split("/en")[1]
     : pathname.startsWith("/ar")
     ? pathname.split("/ar")[1]
     : "/";
-    const lang: lang = pathname.startsWith("/ar") ? "ar" : "en";
-    const dir = lang === "ar" ? "rtl" : "ltr";
-  return {pathname, purePath, lang, dir};
+  const lang: lang = pathname.startsWith("/ar") ? "ar" : "en";
+  // const dir = lang === "ar" ? "rtl" : "ltr";
+  return { pathname, purePath, lang, dir };
 }
 
 export default getPath;
