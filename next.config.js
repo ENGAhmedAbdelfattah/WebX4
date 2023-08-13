548845fa68d1d955633839e6fd01692ace073267
Eng. Ahmed Abdelfattah <92686018+ENGAhmedAbdelfattah@users.noreply.github.com>
2023-08-13 10:10:33 +0300

    last update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true, // use when it fail deploy by type
   },
 };
 
